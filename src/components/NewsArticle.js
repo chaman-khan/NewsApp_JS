de62633224abda9784188d96868bfbfeb3450567
chaman-khan <chaman.devv@gmail.com>
2024-04-27 06:20:41 +0500

    Updated

--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -5,11 +5,24 @@ export class NewsArticle extends Component {
     let { title, description, imageUrl, newsUrl, source } = this.props;
     return (
       <div className="card my-3" style={{ width: "18rem" }}>
-        <img src={imageUrl} className="card-img-top" alt={source.name} />
+        <img
+          src={
+            imageUrl
+              ? imageUrl
+              : "https://www.investors.com/wp-content/uploads/2019/10/stock-wall-street-flags-adobe.jpg"
+          }
+          className="card-img-top"
+          alt={source.name}
+        />
         <div className="card-body">
-          <h5 className="card-title">{title}...</h5>
-          <p className="card-text">{description}...</p>
-          <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
             Read More
           </a>
         </div>
